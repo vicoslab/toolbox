6dad216165c18f4ca548560352b13294693d62bc
Andraž Andolšek Peklaj <andraz.ap@pm.me>
2026-07-09 12:23:23 +0200

    Remove stub server from creation script because gateway exists

--- apps/ls-utils/create.py
+++ apps/ls-utils/create.py
@@ -21,11 +21,8 @@ PROJECT_TITLE = os.getenv('PROJECT_TITLE')
 
 ls = LabelStudio(base_url='http://localhost:8080', api_key=API_KEY)
 
-with open(f'./templates/{TASK}.html') as f:
-    project = ls.projects.create(
-        label_config=f.read(),
-        **{ 'title': PROJECT_TITLE } if PROJECT_TITLE else {},
-    )
+config = Path(f'./templates/{TASK}.html').read_text()
+project = ls.projects.create(label_config=config, **{ 'title': PROJECT_TITLE } if PROJECT_TITLE else {})
 
 print(project.id)
 
@@ -40,34 +37,4 @@ import_storage = ls.import_storage.local.create(
 )
 # TODO: test this once recursive_scan is available, and make api enddpoint set this process to nonblocking
 ls.import_storage.local.sync(import_storage.id)
-
-class Handler(BaseHTTPRequestHandler):
-    def _json(self, payload):
-        self.send_response(200)
-        self.send_header("Content-Type", "application/json")
-        self.end_headers()
-        self.wfile.write(json.dumps(payload).encode())
-
-    def do_GET(self):
-        if self.path == "/health":
-            self._json({"status": "UP"})
-        else:
-            self.send_response(404)
-            self.end_headers()
-
-    def do_POST(self):
-        if self.path == "/setup":
-            self._json({
-                "model_version": "stub",
-                "extra_params": {}
-            })
-        else:
-            length = int(self.headers.get('Content-Length', 0))
-            if length:
-                self.rfile.read(length)
-
-            self._json({})
-
-Thread(target=lambda: HTTPServer(("0.0.0.0", 9090), Handler).serve_forever(), daemon=True).start()
-
 ls.ml.create(title="Inference worker", project=project.id, url="http://localhost:9090")
